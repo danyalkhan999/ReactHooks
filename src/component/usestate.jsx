d7cecc1d83a8ca207b36cf23c66fac708e2e8d0d
Danyal Khan <danyal.khan@ongraph.com>
2025-03-15 11:49:11 +0530

    Button Text

--- src/component/usestate.jsx
+++ src/component/usestate.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const UseState = () => {
-    const [count, setCount] = useState(0)
-    return (
-        <>
-            <button onClick={() => count >= 0 ? setCount((prevCount) => prevCount + 1) : ''}>+</button>
-            <span>{ count }</span>
-            <button onClick={() => count > 0 ? setCount((prevCount) => prevCount - 1) : ''}>-</button>
-        </>
-    )
-}
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button
+        onClick={() =>
+          count >= 0 ? setCount((prevCount) => prevCount + 1) : ""
+        }
+      >
+        Inc. count
+      </button>{" "}
+      <br />
+      <span>Count: {count}</span> <br />
+      <button
+        onClick={() =>
+          count > 0 ? setCount((prevCount) => prevCount - 1) : ""
+        }
+      >
+        Dec. count
+      </button>
+    </>
+  );
+};
 
 export default UseState;
