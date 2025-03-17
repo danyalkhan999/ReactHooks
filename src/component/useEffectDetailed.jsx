953701ae84a214d505ee436b24170dc8bddcba0d
Danyal Khan <danyal.khan@ongraph.com>
2025-03-17 17:49:53 +0530

    useEffect finnished

--- src/component/useEffectDetailed.jsx
+++ src/component/useEffectDetailed.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const UseEffectDetailed = () => {
   const [count, setCount] = useState(0);
-
+  const [total, setTotal] = useState(0);
   /*
     first -> side-efect function
     second -> clean-up function
@@ -17,12 +17,37 @@ const UseEffectDetailed = () => {
 
   // Variation 2: Runs on only first render
 
+  //   useEffect(() => {
+  //     alert("I will run on only first render");
+  //   }, []);
+
+  // variation 3 : runs on dependency
+
+  //   useEffect(() => {
+  //     alert("I wil render on count change");
+  //   }, [count]);
+
+  // variation 3: multiple dependency
+  //   useEffect(() => {
+  //     alert("I will run everytime when count/total is updated");
+  //   }, [count, total]);
+
+  // Variation 4 : let's add up a clean up function
+
   useEffect(() => {
-    alert("I will run on only first render");
-  }, []);
+    alert("Count is updated");
+
+    return () => {
+      alert("count is unmounted from UI");
+    };
+  }, [count]);
 
   const handleClick = () => {
     setCount(count + 1);
+  };
+
+  const handleClickTotal = () => {
+    setTotal(total + 2);
   };
 
   return (
@@ -30,6 +55,10 @@ const UseEffectDetailed = () => {
       <button onClick={handleClick}>Click me</button>
       <br />
       Count is: {count}
+      <br />
+      <button onClick={handleClickTotal}>Update Total</button>
+      <br />
+      Total is: {total}
     </div>
   );
 };
